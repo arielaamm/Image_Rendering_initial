cf421452ee8279d37c257253b1cce455c33b3b4d
Ariel Moreshet <amoreshe@g.jct.ac.il>
2022-12-29 19:55:15 +0200

    Test polygon

--- Exercise1/src/test/PolygonTest.java
+++ Exercise1/src/test/PolygonTest.java
@@ -1,6 +1,9 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 import geometries.Polygon;
@@ -8,28 +11,94 @@ import primitives.Point;
 import primitives.Vector;
 
 public class PolygonTest {
+/**
+	 * Test method for {@link geometries.Polygon#Polygon(primitives.Point...)}.
+	 */
+	@Test
+	public void testConstructor() {
+		// ============ Equivalence Partitions Tests ==============
 
-    /**
-     * @return
-     */
-    @Test
-    void testGetNormal() {
-        Polygon p = new Polygon(
-                new Point(1, 1, 0),
-                new Point(2, 2, 0),
-                new Point(3, 1, 0),
-                new Point(3, 0, 0),
-                new Point(1, -1, 0));
-        Vector expectedNormal = new Vector(0.0, 0.0, 1.0);
-        Vector actualNormal = p.getNormal(new Point(1, 1, 0));
-        assertEquals(expectedNormal, actualNormal);
-        // Polygon p = new Polygon(
-        // new Point(1, 1, 1),
-        // new Point(2, 2, 0),
-        // new Point(3, 1, 0),
-        // new Point(2, 0, 0),
-        // new Point(1, -1, 0));
-        // assertEquals(new Vector (0.577, 0.577, 0.577).normalize(), p.getNormal(new
-        // Point(1, 1, 2)));
-    }
+		// TC01: Correct concave quadrangular with vertices in correct order
+		try {new Polygon(
+			new Point(0, 0, 1),
+			new Point(1, 0, 0),
+			new Point(0, 1, 0), 
+			new Point(-1, 1, 1));
+		} catch (IllegalArgumentException e) {
+			fail("Failed constructing a correct polygon");
+		}
+
+		// TC02: Wrong vertices order
+		assertThrows(IllegalArgumentException.class, //
+				() -> new Polygon(
+					new Point(0, 0, 1),
+					new Point(0, 1, 0), 
+					new Point(1, 0, 0), 
+					new Point(-1, 1, 1)), //
+				"Constructed a polygon with wrong order of vertices");
+
+		// TC03: Not in the same plane
+		assertThrows(IllegalArgumentException.class, //
+				() -> new Polygon(
+					new Point(0, 0, 1), 
+					new Point(1, 0, 0), 
+					new Point(0, 1, 0), 
+					new Point(0, 2, 2)), //
+				"Constructed a polygon with vertices that are not in the same plane");
+
+		// TC04: Concave quadrangular
+		assertThrows(IllegalArgumentException.class, //
+				() -> new Polygon(
+					new Point(0, 0, 1), 
+					new Point(1, 0, 0), 
+					new Point(0, 1, 0),
+					new Point(0.5, 0.25, 0.5)), //
+				"Constructed a concave polygon");
+
+		// =============== Boundary Values Tests ==================
+
+		// TC10: Vertex on a side of a quadrangular
+		assertThrows(IllegalArgumentException.class, //
+				() -> new Polygon(
+					new Point(0, 0, 1), 
+					new Point(1, 0, 0), 
+					new Point(0, 1, 0), 
+					new Point(0, 0.5, 0.5)),
+				"Constructed a polygon with vertix on a side");
+
+		// TC11: Last point = first point
+		assertThrows(IllegalArgumentException.class, //
+				() -> new Polygon(
+					new Point(0, 0, 1), 
+					new Point(1, 0, 0), 
+					new Point(0, 1, 0), 
+					new Point(0, 0, 1)),
+				"Constructed a polygon with vertice on a side");
+
+		// TC12: Co-located points
+		assertThrows(IllegalArgumentException.class, //
+				() -> new Polygon(
+					new Point(0, 0, 1), 
+					new Point(1, 0, 0), 
+					new Point(0, 1, 0), 
+					new Point(0, 1, 0)),
+				"Constructed a polygon with vertice on a side");
+
+	}
+
+	/**
+	 * Test method for {@link geometries.Polygon#getNormal(primitives.Point)}.
+	 */
+	@Test
+	public void testGetNormal() {
+		// ============ Equivalence Partitions Tests ==============
+		// TC01: There is a simple single test here
+		Polygon pl = new Polygon(
+			new Point(0, 0, 1), 
+			new Point(1, 0, 0), 
+			new Point(0, 1, 0), 
+			new Point(-1, 1, 1));
+		double sqrt3 = Math.sqrt(1d / 3);
+		assertEquals(new Vector(sqrt3, sqrt3, sqrt3).normalize(), pl.getNormal(new Point(0, 0, 1)), "Bad normal to trinagle");
+	}
 }
